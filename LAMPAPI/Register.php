eb03e82d818898a4c5f4c0fa3d58cefc99dea2e9
Harry <69062179+khyrdantai@users.noreply.github.com>
2022-05-25 12:37:35 -0400

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,6 +1,11 @@
 <?php
     $inData = getRequestInfo();
 
+    // this is to prevent github from uploading the password
+    $pass_file = fopen("pass", "r");
+    $server_pass = fgets($pass_file);
+    fclose($pass_file);
+
     $conn = new mysqli("localhost", "Admin", $server_pass, "COP4331");
 	if( $conn->connect_error )
 	{
