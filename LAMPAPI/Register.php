95c808afba935bd302dcf0b8631deec5e42a917d
shafferj2019 <94633681+shafferj2019@users.noreply.github.com>
2022-05-24 20:07:23 -0400

    Add Register.php
    File hasn't been tested yet.

---
+++ LAMPAPI/Register.php
@@ -0,0 +1,35 @@
+<?php
+    $inData = getRequestInfo();
+
+    $conn = new mysqli("localhost", "Admin", $server_pass, "COP4331");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+		$stmt->execute();
+        $stmt->close();
+		$conn->close();
+		returnWithError("");
+    }
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+    function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
