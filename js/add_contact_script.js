d99a7dd8acab20683bf9fc01f5e4887f57f34d81
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-05-27 11:39:46 -0400

    Added add contact dialog box
    added add-contact dialog box with buttons

---
+++ js/add_contact_script.js
@@ -0,0 +1,11 @@
+
+    fetch('add_contact.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#show_form");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem,oldelem);
+    })
+
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 function showAlert() {
     var myText = "Did you think there would be somthing here?";
     alert (myText);
-  }
+}
+
+// open pop-up menu for add contact
+function show_add_contact(){
+  var add_row = document.getElementById("add_contact_form");
+  add_row.style = "display: block; width: 100%; margin-left: auto; margin-right: auto;";
+  
+}
+
+// close pop up menu. right now this is more of a cancel than a submit
+function close_add_box(){
+  var add_row = document.getElementById("add_contact_form");
+  add_row.style = "display: none";
+}
+
